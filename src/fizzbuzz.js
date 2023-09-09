04206557426e6f36b1e53ed942d5a45b51a40478
Kye <NovusAa12@proton.me>
2023-09-09 17:59:02 +0100

    completed practice & tests passed

--- src/fizzbuzz.js
+++ src/fizzbuzz.js
@@ -1,4 +1,21 @@
 // TODO: Write your function in this file
+let fizzBuzz = (lower, upper) => {
+    const arr = []
 
+    if (lower > upper) {
+        console.log('ERROR: You must start with a number lower than the upper number')
+    }
+
+    for (i = lower; i <= upper; i++) {
+        (i % 3 === 0 && i % 5 === 0) ? arr.push('FizzBuzz')
+        : (i % 5 === 0 ) ? arr.push('Buzz')
+        : (i % 3 === 0) ? arr.push('Fizz')
+        : (arr.push(i))
+    } 
+
+    return arr
+}
+
+console.log(fizzBuzz(1, 15))
 // TODO: Change undefined below to the name of your function
-module.exports = undefined
+module.exports = fizzBuzz
